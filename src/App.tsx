12e03999c662ec6fa4a51e0563dd4593c74e8211
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-04-16 12:48:34 -0500

    updated Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 import Navbar from "@/scenes/navbar";
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
   return (
     <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar setSelectedPage={setSelectedPage} selectedPage={selectedPage} />
     </div>
   );
 }
